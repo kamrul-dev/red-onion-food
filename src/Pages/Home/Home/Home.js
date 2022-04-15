7260dfcf6bd9078852db404967d85a245d09082b
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-16 01:20:55 +0600

    Header section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <div className='container mx-auto'>
+        <div className='container'>
+            <Header></Header>
             <Banner></Banner>
         </div>
     );
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './Header.css';
-
+import logo2 from '../../../images/logo2.png'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 const Header = () => {
     return (
         <div>
-            <h2>This is header page</h2>            
+            <Navbar collapseOnSelect expand="lg" className='text-dark'>
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img width='80px' src={logo2} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link className='me-2'>
+                                <AiOutlineShoppingCart></AiOutlineShoppingCart>
+                            </Nav.Link>
+                            <Nav.Link className='me-2' >Login</Nav.Link>
+                            <Nav.Link className='sign-up-btn me-2 text-white'>
+                                SignUp
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
