bc73e1237ff666d2f3dbc550571e28d4714df3d1
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-16 00:16:04 +0600

    route setup and banner section done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-
+        <div className='banner-container d-flex align-items-center justify-content-center'>
+            <div className='search-field d-flex'>
+                <input className='form-control' type="search" name="" id="" placeholder='Search your food...' />
+                <Button variant="outline-success">Search</Button>
+            </div>
         </div>
     );
 };
